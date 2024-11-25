c2e6cc31140f5a959d413f43f5b6d6f1d75c88b0
Danielr-1998 <daniel.riano@utp.edu.co>
2024-11-25 13:05:56 -0500

    Front

--- app/services/scraper.py
+++ app/services/scraper.py
@@ -79,7 +79,7 @@ def is_valid_email(email):
     return True
 
 # Función para extraer correos electrónicos de una página
-def scrape_emails(url):
+def scrape_emails1(url):
     emails = set()
     try:
         response = requests.get(url)
@@ -97,3 +97,23 @@ def scrape_emails(url):
         print(f"Error al acceder a la URL: {url}, Error: {e}")
     
     return emails
+
+def scrape_emails(url):
+    """
+    Función que hace scraping de correos electrónicos desde una URL
+    """
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        raise Exception(f"No se pudo acceder a la página: {url}")
+    
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    # Buscar correos electrónicos usando expresión regular
+    emails = set()
+    for a_tag in soup.find_all("a", href=True):
+        email_matches = re.findall(r"[\w\.-]+@[\w\.-]+", a_tag["href"])
+        for email in email_matches:
+            emails.add(email)
+    
+    return list(emails)
